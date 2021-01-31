91c6f6916d27067ef81c5ec1e7bd5240802132a2
jchaconv <jcaaron.chacon@outlook.com>
2021-01-31 00:24:22 -0500

    added project files

---
+++ src/main/java/expert/springframework/Spring5RecetasPeruanasApplication.java
@@ -0,0 +1,13 @@
+package expert.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5RecetasPeruanasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5RecetasPeruanasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/expert/springframework/Spring5RecetasPeruanasApplicationTests.java
@@ -0,0 +1,13 @@
+package expert.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5RecetasPeruanasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
