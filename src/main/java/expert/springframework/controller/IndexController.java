b1b8e54c3567cf153e5ce93f0db2f0159630fbe9
jchaconv <jcaaron.chacon@outlook.com>
2021-02-02 10:39:03 -0500

    added project lombok features.

--- src/main/java/expert/springframework/controller/IndexController.java
+++ src/main/java/expert/springframework/controller/IndexController.java
@@ -1,16 +1,12 @@
 package expert.springframework.controller;
 
-import expert.springframework.domain.Category;
-import expert.springframework.domain.UnitOfMeasure;
-import expert.springframework.repositories.CategoryRepository;
-import expert.springframework.repositories.UnitOfMeasureRepository;
 import expert.springframework.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Optional;
-
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -30,6 +26,8 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index", "/index.html"})
     public String getIndexPage(Model model) {
+
+        log.debug("=== Getting index page ===");
 
         model.addAttribute("recipes", recipeService.getRecipes());
 
