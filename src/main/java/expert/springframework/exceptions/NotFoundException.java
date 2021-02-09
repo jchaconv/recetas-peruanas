cd3d03181f09296782587ed3e3250ffcb19a8016
jchaconv <jcaaron.chacon@outlook.com>
2021-02-09 16:00:26 -0500

    added exception class

---
+++ src/main/java/expert/springframework/exceptions/NotFoundException.java
@@ -0,0 +1,17 @@
+package expert.springframework.exceptions;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException() {
+        super();
+    }
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+    public NotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
